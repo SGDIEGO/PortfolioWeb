b8fdabc842d90bc480a30f298f709d57d9e6eadc
SGDIEGO <diegoalonsosg@gmail.com>
2024-11-04 15:32:27 -0500

    resolve dist deploy folder

--- src/components/Tag/index.tsx
+++
@@ -1,9 +0,0 @@
-interface TagProps {
-    content: string
-}
-
-export default function Tag(props: TagProps) {
-    return <div className="px-4 py-2 bg-palette_200 flex justify-center rounded-2xl">
-        <span>{props.content}</span>
-    </div>
-}
--- src/pages/Contact/index.tsx
+++
@@ -1,7 +0,0 @@
-import Title from "../../components/Title";
-
-export default function Contact() {
-    return <div>
-        <Title content={"Contact"}/>
-    </div>
-}
